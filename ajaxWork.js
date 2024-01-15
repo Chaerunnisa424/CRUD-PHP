68e9243e4d4fdfeb1787e94b581d20568f552c1e
Chaerunnisa Salsabila <117333351+Chaerunnisa424@users.noreply.github.com>
2024-01-15 17:55:33 +0700

    Add files via upload

---
+++ ajaxWork.js
@@ -0,0 +1,339 @@
+
+
+function showProductItems(){  
+    $.ajax({
+        url:"./adminView/viewAllProducts.php",
+        method:"post",
+        data:{record:1},
+        success:function(data){
+            $('.allContent-section').html(data);
+        }
+    });
+}
+function showCategory(){  
+    $.ajax({
+        url:"./adminView/viewCategories.php",
+        method:"post",
+        data:{record:1},
+        success:function(data){
+            $('.allContent-section').html(data);
+        }
+    });
+}
+function showSizes(){  
+    $.ajax({
+        url:"./adminView/viewSizes.php",
+        method:"post",
+        data:{record:1},
+        success:function(data){
+            $('.allContent-section').html(data);
+        }
+    });
+}
+function showProductSizes(){  
+    $.ajax({
+        url:"./adminView/viewProductSizes.php",
+        method:"post",
+        data:{record:1},
+        success:function(data){
+            $('.allContent-section').html(data);
+        }
+    });
+}
+
+
+
+function showOrders(){
+    $.ajax({
+        url:"./adminView/viewAllOrders.php",
+        method:"post",
+        data:{record:1},
+        success:function(data){
+            $('.allContent-section').html(data);
+        }
+    });
+}
+
+function ChangeOrderStatus(id){
+    $.ajax({
+       url:"./controller/updateOrderStatus.php",
+       method:"post",
+       data:{record:id},
+       success:function(data){
+           alert('Order Status updated successfully');
+           $('form').trigger('reset');
+           showOrders();
+       }
+   });
+}
+
+function ChangePay(id){
+    $.ajax({
+       url:"./controller/updatePayStatus.php",
+       method:"post",
+       data:{record:id},
+       success:function(data){
+           alert('Payment Status updated successfully');
+           $('form').trigger('reset');
+           showOrders();
+       }
+   });
+}
+
+
+//add product data
+function addItems(){
+    var nama=$('#nama').val();
+    var alamat=$('#alamat').val();
+    var nomor_kontak=$('#nomor_kontak').val();
+
+    var fd = new FormData();
+    fd.append('nama', nama);
+    fd.append('alamat', alamat);
+    fd.append('nomor_kontak', nomor_kontak);
+    $.ajax({
+        url:"./controller/addItemController.php",
+        method:"post",
+        data:fd,
+        processData: false,
+        contentType: false,
+        success: function(data){
+            alert('Product Added successfully.');
+            $('form').trigger('reset');
+            showProductItems();
+        }
+    });
+}
+
+//edit product data
+function itemEditForm(id){
+    $.ajax({
+        url:"./adminView/editItemForm.php",
+        method:"post",
+        data:{record:id},
+        success:function(data){
+            $('.allContent-section').html(data);
+        }
+    });
+}
+
+// update product after submit
+function updateItems() {
+    var id_tamu = $('#id_tamu').val();
+    var nama = $('#nama').val();
+    var alamat = $('#alamat').val();
+    var nomor_kontak = $('#nomor_kontak').val();
+
+    $.ajax({
+        url: './controller/updateItemController.php',
+        method: 'post',
+        data: {
+            id_tamu: id_tamu,
+            nama: nama,
+            alamat: alamat,
+            nomor_kontak: nomor_kontak
+        },
+        success: function(data) {
+            alert('Data Update Success.');
+            $('form').trigger('reset');
+            showProductItems();
+        },
+        error: function(xhr, status, error) {
+            console.error('AJAX error: ' + status + ' ' + error);
+            alert('Error updating data. Check the console for details.');
+        }
+    });
+}
+
+//delete product data
+function itemDelete(id){
+    $.ajax({
+        url:"./controller/deleteItemController.php",
+        method:"post",
+        data:{record:id},
+        success:function(data){
+            alert('Items Successfully deleted');
+            $('form').trigger('reset');
+            showProductItems();
+        }
+    });
+}
+
+
+//delete cart data
+function cartDelete(id){
+    $.ajax({
+        url:"./controller/deleteCartController.php",
+        method:"post",
+        data:{record:id},
+        success:function(data){
+            alert('Cart Item Successfully deleted');
+            $('form').trigger('reset');
+            showMyCart();
+        }
+    });
+}
+
+function eachDetailsForm(id){
+    $.ajax({
+        url:"./view/viewEachDetails.php",
+        method:"post",
+        data:{record:id},
+        success:function(data){
+            $('.allContent-section').html(data);
+        }
+    });
+}
+
+
+
+//delete category data
+function categoryDelete(id){
+    $.ajax({
+        url:"./controller/catDeleteController.php",
+        method:"post",
+        data:{record:id},
+        success:function(data){
+            alert('Category Successfully deleted');
+            $('form').trigger('reset');
+            showCategory();
+        }
+    });
+}
+
+//delete size data
+function sizeDelete(id){
+    $.ajax({
+        url:"./controller/deleteSizeController.php",
+        method:"post",
+        data:{record:id},
+        success:function(data){
+            alert('Size Successfully deleted');
+            $('form').trigger('reset');
+            showSizes();
+        }
+    });
+}
+
+
+//delete variation data
+function variationDelete(id){
+    $.ajax({
+        url:"./controller/deleteVariationController.php",
+        method:"post",
+        data:{record:id},
+        success:function(data){
+            alert('Successfully deleted');
+            $('form').trigger('reset');
+            showProductSizes();
+        }
+    });
+}
+
+//edit variation data
+function variationEditForm(id){
+    $.ajax({
+        url:"./adminView/editVariationForm.php",
+        method:"post",
+        data:{record:id},
+        success:function(data){
+            $('.allContent-section').html(data);
+        }
+    });
+}
+
+
+//update variation after submit
+function updateVariations(){
+    var v_id = $('#v_id').val();
+    var product = $('#product').val();
+    var size = $('#size').val();
+    var qty = $('#qty').val();
+    var fd = new FormData();
+    fd.append('v_id', v_id);
+    fd.append('product', product);
+    fd.append('size', size);
+    fd.append('qty', qty);
+   
+    $.ajax({
+      url:'./controller/updateVariationController.php',
+      method:'post',
+      data:fd,
+      processData: false,
+      contentType: false,
+      success: function(data){
+        alert('Update Success.');
+        $('form').trigger('reset');
+        showProductSizes();
+      }
+    });
+}
+function search(id){
+    $.ajax({
+        url:"./controller/searchController.php",
+        method:"post",
+        data:{record:id},
+        success:function(data){
+            $('.eachCategoryProducts').html(data);
+        }
+    });
+}
+
+
+function quantityPlus(id){ 
+    $.ajax({
+        url:"./controller/addQuantityController.php",
+        method:"post",
+        data:{record:id},
+        success:function(data){
+            $('form').trigger('reset');
+            showMyCart();
+        }
+    });
+}
+function quantityMinus(id){
+    $.ajax({
+        url:"./controller/subQuantityController.php",
+        method:"post",
+        data:{record:id},
+        success:function(data){
+            $('form').trigger('reset');
+            showMyCart();
+        }
+    });
+}
+
+function checkout(){
+    $.ajax({
+        url:"./view/viewCheckout.php",
+        method:"post",
+        data:{record:1},
+        success:function(data){
+            $('.allContent-section').html(data);
+        }
+    });
+}
+
+
+function removeFromWish(id){
+    $.ajax({
+        url:"./controller/removeFromWishlist.php",
+        method:"post",
+        data:{record:id},
+        success:function(data){
+            alert('Removed from wishlist');
+        }
+    });
+}
+
+
+function addToWish(id){
+    $.ajax({
+        url:"./controller/addToWishlist.php",
+        method:"post",
+        data:{record:id},
+        success:function(data){
+            alert('Added to wishlist');        
+        }
+    });
+}
